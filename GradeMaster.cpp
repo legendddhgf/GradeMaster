8a2a834fa0e1322279e9d2f659a5f760d9fa6414
August Valera <4U6U57@gmail.com>
2017-12-29 12:31:30 -0800

    Add Interface module, import Taywee/args

--- GradeMaster.cpp
+++ GradeMaster.cpp
@@ -4,12 +4,14 @@
 
 #include "ExtraUtil.h"
 
-#include"AssignmentInfo.h"
+#include "Interface.hpp"
+#include "AssignmentInfo.h"
 
 #ifdef OVERRIDE_PRINTF
 #define printf(...) normal_printf(__VA_ARGS__)
 #endif
 
 int main(int argc, char **argv) {
-  printf("Don't you wish that it worked too?\n");
+  //printf("Don't you wish that it worked too?\n");
+  cli_main(argc, argv);
 }
